c03588a946f2254056c823fa1d7135fadbba4ad1
Joseph Yan'dev <joedevelopermiester@gmail.com>
2017-01-01 20:02:08 -0500

    Added step 2 of world gen - untested

--- Assets/Code/Main/WorldData.cs
+++ Assets/Code/Main/WorldData.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 [System.Serializable]
 public class WorldData {
@@ -14,13 +15,33 @@ public class WorldData {
         /* Generates a new world and fills the w_Zones property.
          */
 
-        ZoneData[,] newWorldZones = new ZoneData[length, width]; //Create a 2D Array of blank zones.
+        // Create a 2D Array of blank zones:
+        ZoneData[,] newWorldZones = new ZoneData[length, width];
         for (int yIndex = 0; yIndex < width; yIndex++) {
             for (int xIndex = 0; xIndex < length; xIndex++) {
                 newWorldZones[yIndex, xIndex] = new ZoneData();
                 newWorldZones[yIndex, xIndex].z_ZoneType = new ZoneType(); //This is automatically set to unspecified.
             }
         }
-
+        // ---
+        // Pick random starting points for each zone type.
+        foreach (ZoneType currentType in zoneTypes.zoneTypes) {
+            Vector2 chosenPoint = pickRandomUnchosenPoint(currentType.name, newWorldZones);
+            newWorldZones[(int)chosenPoint.x, (int)chosenPoint.y].z_ZoneType = currentType;
+        }
     }
+
+    #region --- [World-Gen Helpers] ---
+    private Vector2 pickRandomUnchosenPoint (string zoneType, ZoneData[,] zones) {
+        var chosen = false;
+        Vector2 randomVector = Vector2.zero;
+        while (chosen == false) {
+            randomVector = new Vector2((int)Random.Range(0, zones.GetLength(0)), (int)Random.Range(0, zones.GetLength(1)));
+            if (zones[(int)randomVector.x, (int)randomVector.y].z_ZoneType.name != zoneType) {
+                chosen = true;
+            }
+        }
+        return randomVector;
+    }
+    #endregion
 }
